359806ac64cfa8906be812f3b077ca61da5f5e2e
gtz.jesus <gtz.jesus@outlook.com>
2023-03-30 15:14:04 -0600

    Navigation Implementation for mobile responsiveness📲

--- src/js/helpers/menu.js
+++ src/js/helpers/menu.js
@@ -1,6 +1,17 @@
+/**
+ * HELPER EVENT LISTENER THAT HANDLES THE NAVIGATION (HAMBURGER)
+ * MENU RESPONSE FOR MOBILE MENU (RESPONSIVE HEADER)
+ */
+
+//// GRAB NECESSARY SELECTORS TO HANDLE THE USER'S CLICK EVENT
 const primaryNavigation = document.querySelector('.primary__navigation');
 const navigationToggle = document.querySelector('.mobile__nav__toggle');
 
+/**
+ * EVENT LISTENER HANDLER ADDED TO OUR BUTTON IN ORDER TO TRIGGER
+ * OUR CSS(_HEADER.CSS) DATA-VISIBLE ATTRIBUTES (false/true)
+ * @returns NULL HANDLES ONLY THE CLICK EVENT
+ */
 navigationToggle.addEventListener('click', function () {
   const visibility = primaryNavigation.getAttribute('data-visible');
   console.log(visibility);
--- src/js/views/headerView.js
+++ src/js/views/headerView.js
@@ -1,10 +1,15 @@
+/**
+ * HEADER IMPLEMENTATION FILE
+ */
+
 import View from './View';
 import spectrum from '../../imgs/logo.png';
 import search from '../../imgs/search.png';
 
 class HeaderView extends View {
+  //// GRAB PARENT ELEMENT TO DISPLAY HTML MARKUP
   _parentElement = window.document.querySelector('.header');
-
+  //// GENERATE THE MARKUP THAT RETURNS THE HTML TO RENDER
   _generateMarkup() {
     return `
         <div class="logo">
